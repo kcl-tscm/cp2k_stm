636be8749d07006fccb008f144272ee4a3fbfb23
Alejandro Santana-Bonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2022-09-16 14:38:22 +0100

    Add files via upload

---
+++ job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#$ -l h_rt=48:00:00
+#$ -l mem=4.5G
+#$ -pe mpi 40
+#$ -N cp2k_h20
+#$ -A KCL_Admin_rse
+#$ -P Gold
+#$ -cwd 
+
+module purge
+module load gerun
+module load gcc-libs
+module load compilers/gnu/4.9.2
+module load mpi/openmpi/3.1.4/gnu-4.9.2
+module load openblas/0.3.7-openmp/gnu-4.9.2
+module load cp2k/7.1/ompi/gnu-4.9.2
+
+export OMP_NUM_THREADS=1
+gerun cp2k.popt -inp new.inp > output.out  
+
+
+
+
